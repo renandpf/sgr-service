5a238798d47f10c12981d1d51fa03384d149ac90
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-04 15:10:30 -0300

    feat: desenvolvendo getProdutoByCategoria

--- src/gerencial/core/application/ports/ProdutoRepositoryGateway.ts
+++ src/gerencial/core/application/ports/ProdutoRepositoryGateway.ts
@@ -1,5 +1,7 @@
+import { Categoria } from "../../domain/Categoria";
 import { Produto } from "../../domain/Produto";
 
 export interface IProdutoRepositoryGateway {
     obterById(id: number): Promise<Produto>;
+    obterByCategoria(categoria: Categoria): Promise<Produto[]>;
 }
--- src/gerencial/core/application/useCases/produtoUseCases/ObterProduto.spec.ts
+++ src/gerencial/core/application/useCases/produtoUseCases/ObterProduto.spec.ts
@@ -4,15 +4,16 @@ import { IProdutoRepositoryGateway } from "../../ports/ProdutoRepositoryGateway"
 import { anyProduto } from "../../../../../__tests__/databuilder/ProductDatabuilder";
 import { anyNumber } from "../../../../../__tests__/databuilder/PrimitiveDatabuilder";
 import { ObterProdutoUseCase } from "./ObterProduto";
+import { Categoria } from "../../../domain/Categoria";
 
 describe("Testes de ObterProduto", () => {
   beforeEach(PlatformTest.create);
   afterEach(PlatformTest.reset);
 
-  const anyProductId: number = anyNumber.build();
-  const produtoExpected = anyProduto.build();
   
   it("deve obter um produto pelo id", async () => {
+    const anyProductId: number = anyNumber.build();
+    const produtoExpected = anyProduto.build();
     const mockedProdutoRepositoryGateway = mock<IProdutoRepositoryGateway>();
     mockedProdutoRepositoryGateway.obterById.calledWith(anyProductId).mockResolvedValue(produtoExpected);
 
@@ -22,4 +23,21 @@ describe("Testes de ObterProduto", () => {
 
     expect(produtoExpected).toEqual(produtoFound);
   });
+
+  it("deve obter todos produtos a partir de uma categoria", async () => {
+    const anyCategoria = Categoria.ACOMPANHAMENTO;
+    const anyProdutoA = anyProduto.build();
+    const anyProdutoB = anyProduto.build();
+    const produtosExistents = [anyProdutoA, anyProdutoB];
+
+    const mockedProdutoRepositoryGateway = mock<IProdutoRepositoryGateway>();
+    mockedProdutoRepositoryGateway.obterByCategoria.calledWith(anyCategoria).mockResolvedValue(produtosExistents);
+
+    const obterProdutoUseCase = new ObterProdutoUseCase(mockedProdutoRepositoryGateway);
+
+    const produtosFound = await obterProdutoUseCase.obterPorCategoria(anyCategoria);
+
+    expect(produtosExistents).toEqual(produtosFound);
+  });
+
 });
