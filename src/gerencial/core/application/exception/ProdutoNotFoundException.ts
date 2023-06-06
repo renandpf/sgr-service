5f8d7df8ac9880aaadd54f809e30843d3ceef0d3
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-06 14:39:59 -0300

    refact: adicionando optional e codando caso de erro: produto não encontrado

--- src/gerencial/core/application/exception/ProdutoNotFoundException.ts
+++ src/gerencial/core/application/exception/ProdutoNotFoundException.ts
@@ -1,7 +1,7 @@
 import { SystemBaseException } from "./SystemBaseException";
 
 export class ProdutoNotFoundException extends SystemBaseException {
-    public code = "sgr.produtoNotFound";
-    public message = "Produto não foi encontrado";
-    public httpStatus = 404;
+    public readonly code = "sgr.produtoNotFound";
+    public readonly message = "Produto não foi encontrado";
+    public readonly httpStatus = 404;
 }
