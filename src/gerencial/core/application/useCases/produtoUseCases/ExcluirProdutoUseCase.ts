be2ac7fd110121612547e272da9b712adb41d226
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-06 21:29:20 -0300

    doc: adicionando comentário de TODO

--- src/gerencial/core/application/useCases/produtoUseCases/ExcluirProdutoUseCase.ts
+++ src/gerencial/core/application/useCases/produtoUseCases/ExcluirProdutoUseCase.ts
@@ -6,6 +6,7 @@ export class ExcluirProdutoUseCase {
     constructor(@Inject() private produtoRepositoryGateway: IProdutoRepositoryGateway) { }
 
     public async excluir(produto: Produto): Promise<void> {
+        //TODO: adicionar a regra: caso o produto esteja associado a pelo menos 1 pedido, não deve ser excluído.
         this.produtoRepositoryGateway.excluir(produto);
     }
 }
