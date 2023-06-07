c22b5ddeb2c8aa2b46fd371543b830636fda991a
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-06 21:35:13 -0300

    doc: removendo comentario

--- src/gerencial/adapter/driver/controllers/produto/ProdutoController.ts
+++ src/gerencial/adapter/driver/controllers/produto/ProdutoController.ts
@@ -19,7 +19,7 @@ export class ProdutoController {
   }
   @Get("/categorias/:categoria/produtos")
   @Returns(200, Array<ProdutoJson>)
-  async obterPorCategoria(@PathParams("categoria") categoria: CategoriaEnum): Promise<ProdutoJson[]> {//TODO: testar se enum funciona aqui
+  async obterPorCategoria(@PathParams("categoria") categoria: CategoriaEnum): Promise<ProdutoJson[]> {
    console.log(categoria);
     const produtos = await this.obterProdutoUseCase.obterPorCategoria(categoria);
    return produtos.map(p => new ProdutoJson(p));
