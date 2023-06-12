ce028a02448bdd02c42a46d1c22ebd8f2615ddbc
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-12 15:47:18 -0300

    feat

--- src/gerencial/adapter/driven/repositories/ProdutoMySqlRepositoryGateway.ts
+++ src/gerencial/adapter/driven/repositories/ProdutoMySqlRepositoryGateway.ts
@@ -3,10 +3,11 @@ import { IProdutoRepositoryGateway } from "../../../core/application/ports";
 import { Produto, CategoriaEnum } from "../../../core/domain";
 import { Optional } from "typescript-optional";
 import { MYSQL_DATA_SOURCE } from "../../../../config/database/MysqlDataSource";
-import { DataSource, In } from "typeorm";
+import { DataSource, Equal, In } from "typeorm";
 import { ProdutoEntity } from "../../../../common";
 import { ErrorToAccessDatabaseException } from "../../exception/ErrorToAccessDatabaseException";
 import { PRODUTO_DATABASE_REPOSITORY } from "../../../../config/database/repository/repository-register.provider";
+import { equal } from "assert";
 
 @Service()
 export class ProdutoMySqlRepositoryGateway implements IProdutoRepositoryGateway {
@@ -31,7 +32,8 @@ export class ProdutoMySqlRepositoryGateway implements IProdutoRepositoryGateway 
     }
     async obterPorCategoria(categoria: CategoriaEnum): Promise<Produto[]> {
         try {
-            const produtosEntities = await this.produtoRepository.find();//FIXME: filtrar pela categoria
+            const position = (CategoriaEnum as never)[categoria] + 1;
+            const produtosEntities = await this.produtoRepository.findBy({categoriaId: Equal(position)});
             return produtosEntities.map(pe => pe.getDomain());
 
         } catch (e) {
