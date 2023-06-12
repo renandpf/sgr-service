ef61bb76ed63350cab89944f0dcf84972673a29d
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-12 17:16:10 -0300

    chore: adicionando logs

--- src/gerencial/core/application/useCases/produtoUseCases/CriarProdutoUseCase.ts
+++ src/gerencial/core/application/useCases/produtoUseCases/CriarProdutoUseCase.ts
@@ -2,12 +2,19 @@ import { Inject, Service } from "@tsed/common";
 import { IProdutoRepositoryGateway } from "../../ports";
 import { Produto } from "../../../domain";
 import { ProdutoMySqlRepositoryGateway } from "../../../../adapter";
+import { Logger } from "@tsed/common";
 
 @Service()
 export class CriarProdutoUseCase {
     constructor( @Inject(ProdutoMySqlRepositoryGateway) private produtoRepositoryGateway: IProdutoRepositoryGateway ){}
 
+    @Inject()
+    logger: Logger;
+
     public async criar(produto: Produto): Promise<number> {
-        return this.produtoRepositoryGateway.criar(produto);
+        this.logger.trace("Start produto={}", produto);
+        const id = this.produtoRepositoryGateway.criar(produto);
+        this.logger.trace("End id={}", id);
+        return id;
     }
 }
