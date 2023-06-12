778ba30d5575905ed20697dd953993c0b549642f
Renan Puppo Furtado <renan.puppo@gmail.com>
2023-06-12 17:22:14 -0300

    chore: adicionando logs

--- src/gerencial/core/application/useCases/produtoUseCases/ExcluirProdutoUseCase.ts
+++ src/gerencial/core/application/useCases/produtoUseCases/ExcluirProdutoUseCase.ts
@@ -1,12 +1,18 @@
 import { Inject } from "@tsed/common";
 import { IProdutoRepositoryGateway } from "../../ports";
 import { ProdutoMySqlRepositoryGateway } from "../../../../adapter";
+import { Logger } from "@tsed/common";
 
 export class ExcluirProdutoUseCase {
     constructor(@Inject(ProdutoMySqlRepositoryGateway) private produtoRepositoryGateway: IProdutoRepositoryGateway) { }
 
+    @Inject()
+    logger: Logger;
+
     public async excluir(id: number): Promise<void> {
+        this.logger.trace("Start id={}", id);
         //TODO: adicionar a regra: caso o produto esteja associado a pelo menos 1 pedido, não deve ser excluído.
         await this.produtoRepositoryGateway.excluir(id);
+        this.logger.trace("End");
     }
 }
