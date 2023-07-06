76eea51516d60f2dec5c3ea7183ca8e4427a2d0d
André Paes França <andrepaesfranca@gmail.com>
2023-07-06 16:46:11 -0300

    fix: correção das variaveis de ambiente

--- src/config/database/MysqlDataSource.ts
+++ src/config/database/MysqlDataSource.ts
@@ -11,11 +11,11 @@ export const MysqlDataSource = new DataSource({
   // name: "default",  if you come from v0.2.x
   type: "mysql",
   entities: [ProdutoEntity, ClienteEntity, PedidoEntity, ItemEntity, PagamentoEntity], // add this to discover typeorm model
-  host: process.env.HOST || "localhost", // Usar esse host para conectar as aplicação em container ao banco em container
+  host: process.env.DB_HOST || "localhost", // Usar esse host para conectar as aplicação em container ao banco em container
   port: db_port,
-  username: process.env.USERNAME || "root",
-  password: process.env.PASSWORD || "senha",
-  database: process.env.DATABASE || "sgr_database",
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "senha",
+  database: process.env.DB_SCHEMA || "sgr_database",
   synchronize: true,
 });
 
